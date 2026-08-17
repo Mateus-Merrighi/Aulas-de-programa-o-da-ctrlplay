02e00f32b29e1a380a343b9b66ca7a6aae21bf4f
Mateus Merrighi <mateusmerrighi@gmail.com>
2026-08-17 15:38:38 -0300

    17/08/26

---
+++ Ctrl young 2/Aula 3/aula3 atividade.js
@@ -0,0 +1,5 @@
+let p = document.getElementById("status")
+let b = document.getElementById("btn-assistir")
+b.onclick = function(){ 
+    p.innerHTML = "Já assisti, por favor não sequesre meus familiares"
+}
